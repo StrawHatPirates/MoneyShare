23512b149d66ad759ad2cf8189cd65d6ff73634c
Parvez Ahmed <parvezthepop@gmail.com>
2015-06-14 14:21:47 +0530

    Merged all helpers into one signle helper

--- src/app/src/main/java/com/tachys/moneyshare/dataaccess/db/MoneyShareDbHelper.java
+++ src/app/src/main/java/com/tachys/moneyshare/dataaccess/db/MoneyShareDbHelper.java
@@ -5,14 +5,29 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import com.tachys.moneyshare.dataaccess.db.contracts.ExpenseContract;
 import com.tachys.moneyshare.dataaccess.db.contracts.ExpenseMemberContract;
+import com.tachys.moneyshare.dataaccess.db.contracts.MemberContract;
 
-public class ExpenseMemberDbHelper extends SQLiteOpenHelper {
-    private static final String LOG_TAG = "ExpenseMemberDbHelper";
+public class MoneyShareDbHelper extends SQLiteOpenHelper {
+
+    private static final String LOG_TAG = "MoneyShareDbHelper";
     private static final String TEXT_TYPE = " TEXT";
+    private static final String COMMA_SEP = ",";
     private static final String REAL_TYPE = " REAL";
     private static final String INTEGER_TYPE = " INTEGER";
-    private static final String COMMA_SEP = ",";
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "MoneyShare.db";
+
+    private static final String SQL_CREATE_EXPENSES =
+            "CREATE TABLE IF NOT EXISTS " + ExpenseContract.ExpenseEntry.TABLE_NAME + " ("
+                    + ExpenseContract.ExpenseEntry._ID + " INTEGER PRIMARY KEY,"
+                    + ExpenseContract.ExpenseEntry.COLUMN_NAME_Name + TEXT_TYPE + COMMA_SEP + " )";
+
+    private static final String SQL_DELETE_EXPENSES =
+            "DROP TABLE IF EXISTS " + ExpenseContract.ExpenseEntry.TABLE_NAME;
+
     private static final String SQL_CREATE_EXPENSEMEMBERS =
             "CREATE TABLE IF NOT EXISTS " + ExpenseMemberContract.ExpenseMemberEntry.TABLE_NAME + " ("
                     + ExpenseMemberContract.ExpenseMemberEntry._ID + " INTEGER PRIMARY KEY,"
@@ -24,17 +39,25 @@ public class ExpenseMemberDbHelper extends SQLiteOpenHelper {
     private static final String SQL_DELETE_EXPENSEMEMBERS =
             "DROP TABLE IF EXISTS " + ExpenseMemberContract.ExpenseMemberEntry.TABLE_NAME;
 
-    public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "MoneyShare.db";
+    private static final String SQL_CREATE_MEMBERS =
+            "CREATE TABLE IF NOT EXISTS " + MemberContract.MemberEntry.TABLE_NAME + " ("
+                    + MemberContract.MemberEntry._ID + " INTEGER PRIMARY KEY,"
+                    + MemberContract.MemberEntry.COLUMN_NAME_Name + TEXT_TYPE + COMMA_SEP
+                    + MemberContract.MemberEntry.COLUMN_NAME_Email + TEXT_TYPE + COMMA_SEP
+                    + MemberContract.MemberEntry.COLUMN_NAME_Phone + TEXT_TYPE + COMMA_SEP + " )";
 
-    public ExpenseMemberDbHelper(Context context) {
+    private static final String SQL_DELETE_MEMBERS =
+            "DROP TABLE IF EXISTS " + MemberContract.MemberEntry.TABLE_NAME;
+
+    public MoneyShareDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
         Log.i(LOG_TAG, "Creating Table if not exists");
+        db.execSQL(SQL_CREATE_EXPENSES);
+        db.execSQL(SQL_CREATE_MEMBERS);
         db.execSQL(SQL_CREATE_EXPENSEMEMBERS);
     }
 
@@ -42,6 +65,8 @@ public class ExpenseMemberDbHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // The local DB is going to be just a cache. The APP will fetch all the data from the server again.
         Log.i(LOG_TAG, "Deleting Table if exists");
+        db.execSQL(SQL_DELETE_EXPENSES);
+        db.execSQL(SQL_DELETE_MEMBERS);
         db.execSQL(SQL_DELETE_EXPENSEMEMBERS);
         onCreate(db);
     }
